235b9b01025265e9413ff9bb0f1dfec762e3b7c5
chrislupdx <christopherlu.pdx@gmail.com>
2023-03-08 22:57:34 -0800

    app connects to sql db

--- control/app/app.py
+++ control/app/app.py
@@ -1,3 +1,33 @@
-#we are using this to probably populate the database after the db gets inited
+import psycopg2
 
 print("susan")
+
+#connect to the database
+def init():
+    conn = psycopg2.connect(
+            dbname='postgres',
+            user='postgres',
+            port='5432',
+            host='localhost',
+            password='example'
+            )
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+
+    #populate the table
+    for i in range(1000):
+        print(i)  
+
+    pass
+
+#bench
+def bench():
+    
+    #time return time
+    #for i in 1000();
+        #GET thing
+    #get an average
+    #get a distribution
+    pass
+
+init()
