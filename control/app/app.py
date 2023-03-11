4ef58f1e6b639b46a2a855972ccf18e52e9ed4c8
chrislupdx <christopherlu.pdx@gmail.com>
2023-03-11 14:37:05 -0800

    adding stuff into the db correctly

--- control/app/app.py
+++ control/app/app.py
@@ -7,17 +7,38 @@ print("susan")
 #connect to the database
 def init():
     conn = psycopg2.connect(
-            dbname='postgres',
+            dbname='postgres_database',
             user='user0',
             port='5432',
             host='localhost',
             password='example'
             )
     cur = conn.cursor()
-    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
 
-    result = cur.fetchall()
-    print("result set: ", "\n", result)
+    #check if CONTROL_TABLE EXISTS
+        #IF IT DOES DROP IT
+    if_table_exists_drop = '''
+    DROP TABLE IF EXISTS CONTROL_TABLE
+    '''
+    cur.execute(if_table_exists_drop)
+
+    create_table_sql = '''CREATE TABLE CONTROL_TABLE(
+        ENTRY_ID INT,
+        ENTRY_VALUE INT
+    );'''
+    cur.execute(create_table_sql)
+    
+    sample_create_number = 100
+    for i in range(sample_create_number):
+        create_entry_sql = 'INSERT INTO CONTROL_TABLE (ENTRY_ID, ENTRY_VALUE) VALUES ({id}, {entry_val});'.format(id = i, entry_val = i)
+        cur.execute(create_entry_sql)
+
+    conn.commit()
+
+    #thid display isn't working
+    cur.execute("select * from CONTROL_TABLE;")
+    # result = cur.fetchall()
+    # print("result set: ", "\n", result)
 
     #display table
     # cur.execute("\\dt")    
