44b36e5224cc19614e480803a708ad8a24640b5a
chrislupdx <christopherlu.pdx@gmail.com>
2023-03-11 14:58:10 -0800

    started building out bench read/write + timining +statistics

--- control/app/app.py
+++ control/app/app.py
@@ -6,6 +6,7 @@ print("susan")
 
 #connect to the database
 def init():
+    print("starting init")
     conn = psycopg2.connect(
             dbname='postgres_database',
             user='user0',
@@ -15,8 +16,7 @@ def init():
             )
     cur = conn.cursor()
 
-    #check if CONTROL_TABLE EXISTS
-        #IF IT DOES DROP IT
+    #check if CONTROL_TABLE EXISTS, IF IT DOES DROP IT
     if_table_exists_drop = '''
     DROP TABLE IF EXISTS CONTROL_TABLE
     '''
@@ -37,26 +37,32 @@ def init():
     cur.execute("select * from CONTROL_TABLE;")
     res = cur.fetchall()
     print("result set: ", "\n", res)
-
-    #display table
-    # cur.execute("\\dt")    
-    # res = subprocess.run('psql -c "\d+ my_table" test postgres', stdout=subprocess.PIPE)
-    # print(res.stdout.decode(sys.stdout.encoding))
-    #populate the table
-    # for i in range(1000):
-        #create entries
-        # print(i)  
-
     pass
 
 #bench
 def bench():
-    
-    #time return time
-    #for i in 1000();
-        #GET thing
-    #get an average
+    print("staritng bench")
+    #measures the performance of the db in terms of reads and writes
+
+    #first connect to the db
+    conn = psycopg2.connect(
+            dbname='postgres_database',
+            user='user0',
+            port='5432',
+            host='localhost',
+            password='example'
+            )
+    cur = conn.cursor()
+
+    #TRY: if the db exists
+
+    #BENCH 1: read every value from the database
+
+    #BENCH 2: re-write every value from database 
+
+    #get an average and distribution of these metrics across 150 runs
     #get a distribution
     pass
 
 init()
+bench()
