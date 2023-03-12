d4cef17a9d94c327c800fd8818cb13c6838a32a3
chrislupdx <christopherlu.pdx@gmail.com>
2023-03-11 18:58:38 -0800

    before cleanup

--- control/app/app.py
+++ control/app/app.py
@@ -29,6 +29,7 @@ def init():
     );'''
     cur.execute(create_table_sql)
     
+    #we could throw booleean flags for read and writen
     sample_create_number = 10
     for i in range(sample_create_number):
         create_entry_sql = 'INSERT INTO CONTROL_TABLE (ENTRY_ID, ENTRY_VALUE) VALUES ({id}, {entry_val});'.format(id = i, entry_val = i)
@@ -40,12 +41,11 @@ def init():
     print("result set: ", "\n", res)
     return sample_create_number
 
-#bench
 def bench(sample_number):
     print("starting bench")
     #measures the performance of the db in terms of reads and writes
 
-    #first connect to the db
+    #first connect to the db/do this for each database
     conn = psycopg2.connect(
             dbname='postgres_database',
             user='user0',
@@ -55,26 +55,37 @@ def bench(sample_number):
             )
     cur = conn.cursor()
 
-    #BENCH 1: read every value from the database
-    #this might only measure the front of house's speed at generating these requests
+    #BENCH 1: read every value from the database, this might only measure the front of house's speed at generating these requests
     read_times = []
     for i in range(sample_number):
-        add = 'SELECT * FROM CONTROL_TABLE WHERE ENTRY_ID = {id};'.format(id = i)
-        cur.execute(add)
-        res = cur.fetchall()
+        add = 'SELECT * FROM CONTROL_TABLE WHERE ENTRY_ID = \'{id}\''.format(id = i)
+        # cur.execute(add)
+        # res = cur.fetchall()
+        # print(res)
+
+        val = 'cur.execute(' + add + ')'
+        res = timeit.timeit(stmt=val, number=1)
+
+        # res = cur.fetchall()
         print(res)
 
+        # cmd = 'cur.execute(' + add + ' res= cur.fetchall())'
+        # cmd = 'cur.execute(\'' + add + ';\')' #this is going to be a string how do we break #1719: putting quotes between the string didn't satisfy the syntax rules
+        # cmd = 'cur.execute(\'' + add + ';\')'
+        # print(cmd)
+        # timeit.timeit(cmd)
         #this is the interpolated version we feed into timeit
         # cmd = 'curr.execute({sqladd})'.format(sqladd = add)
-        # print('cmd is: ', cmd)
-        
+        # print('cmd is: ', cmd)        
         # one_read_time = timeit.timeit(cmd)
         # read_times.append(one_read_time)   
 
-    #mean, max, min
+        #mean, max, min
+        #get the mean of read_times
+        #get the min of read_times
+        #get the max of read_times
 
     #BENCH 2: re-write every value from database 
-
     #get an average and distribution of these metrics across 150 runs
     #get a distribution
     pass
