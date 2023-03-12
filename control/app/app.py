9b07037406ae3c23cb285a790fcb71f47bb0a9d1
chrislupdx <christopherlu.pdx@gmail.com>
2023-03-11 19:37:33 -0800

    gave up on timeit

--- control/app/app.py
+++ control/app/app.py
@@ -1,11 +1,8 @@
 import psycopg2
 import sys
 import subprocess
-import timeit
+import timeit, time
 
-print("susan")
-
-#connect to the database
 def init():
     print("starting init")
     conn = psycopg2.connect(
@@ -29,7 +26,7 @@ def init():
     );'''
     cur.execute(create_table_sql)
     
-    #we could throw booleean flags for read and writen
+    #we could throw booleean flags for read and writen                                 
     sample_create_number = 10
     for i in range(sample_create_number):
         create_entry_sql = 'INSERT INTO CONTROL_TABLE (ENTRY_ID, ENTRY_VALUE) VALUES ({id}, {entry_val});'.format(id = i, entry_val = i)
@@ -42,10 +39,7 @@ def init():
     return sample_create_number
 
 def bench(sample_number):
-    print("starting bench")
-    #measures the performance of the db in terms of reads and writes
-
-    #first connect to the db/do this for each database
+    print("starting bench...")
     conn = psycopg2.connect(
             dbname='postgres_database',
             user='user0',
@@ -59,37 +53,14 @@ def bench(sample_number):
     read_times = []
     for i in range(sample_number):
         add = 'SELECT * FROM CONTROL_TABLE WHERE ENTRY_ID = \'{id}\''.format(id = i)
-        # cur.execute(add)
-        # res = cur.fetchall()
-        # print(res)
-
-        val = 'cur.execute(' + add + ')'
-        res = timeit.timeit(stmt=val, number=1)
-
-        # res = cur.fetchall()
-        print(res)
-
-        # cmd = 'cur.execute(' + add + ' res= cur.fetchall())'
-        # cmd = 'cur.execute(\'' + add + ';\')' #this is going to be a string how do we break #1719: putting quotes between the string didn't satisfy the syntax rules
-        # cmd = 'cur.execute(\'' + add + ';\')'
-        # print(cmd)
-        # timeit.timeit(cmd)
-        #this is the interpolated version we feed into timeit
-        # cmd = 'curr.execute({sqladd})'.format(sqladd = add)
-        # print('cmd is: ', cmd)        
-        # one_read_time = timeit.timeit(cmd)
-        # read_times.append(one_read_time)   
-
-        #mean, max, min
-        #get the mean of read_times
-        #get the min of read_times
-        #get the max of read_times
-
-    #BENCH 2: re-write every value from database 
-    #get an average and distribution of these metrics across 150 runs
-    #get a distribution
+        tic = time.perf_counter()
+        cur.execute(add)
+        res = cur.fetchall()
+        toc = time.perf_counter()
+        time_elapsed_for_one = toc - tic
+        read_times.append(time_elapsed_for_one)
+    print('times are ', read_times)
     pass
 
 sample_num = init()
-#we might want more mechanical flexibility here
 bench(sample_num)
